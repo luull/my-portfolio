2f657deffff8e86c331dd61bab97d0bdf8434c07
luull <assalafy@steradian.co.id>
2024-09-13 23:41:50 +0700

    fixing photo not showing part 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -91,11 +91,11 @@ export default function Home() {
                 className="w-full mt-4"
               >
                 <Image
-                  src="/Images/me.jpg"
+                  src="./images/me.jpg"
                   height="1000"
                   width="1000"
                   className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-                  alt="thumbnail"
+                  alt="foto"
                 />
               </CardItem>
 
